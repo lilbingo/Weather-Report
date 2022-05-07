ec514620d3352a6d601f4b28b2649122fe67379f
Cameron Helton <cameron.helton@icloud.com>
2022-05-07 18:25:15 -0400

    Combined location.py and weather.py

--- weather.py
+++ weather.py
@@ -1,25 +1,31 @@
 import requests
-from location import get_location
 
+# Uses zipcode to get location information from get_location then sends data to get_weather
 def get_zipcode():
     zipcode = input("Enter your zipcode: ")
     location = get_location(zipcode)
     get_weather(location)
 
 
-"""NOTE: You must get your own api key. See README"""
+# Gets latitude, longitude, city, and state from zipcode api and sends data back to get_zipcode
+def get_location(zip: str) -> str:
+    api_key = ""
+    url = f"https://www.zipcodeapi.com/rest/{api_key}/info.json/{zip}/degrees"
+    loc_json = requests.get(url).json()
+    loc_data = [loc_json["lat"], loc_json["lng"], loc_json["city"], loc_json["state"]]
+    return loc_data
+
+
+# Gets weather data from openweathermap using location data from get_location
 def get_weather(location: str):
     api_key = ""
-    # location[0] = latitude 
-    # location[1] = longitude
-    # location[2] = city 
-    # location[3] = state
     url = f'https://api.openweathermap.org/data/2.5/onecall?lat={location[0]}&lon={location[1]}' \
           f'&units=imperial&exclude=minutely&appid={api_key}'
     data = requests.get(url).json()
     print_weather(data, location[2], location[3])  
 
 
+# Displays weather data received in get_weather
 def print_weather(data: dict, city: str, state: str):
     print(f"\nYour location: {city.title()}, {state.upper()}")
     print(f'It is currently {data["current"]["temp"]} degrees '
